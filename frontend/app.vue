080dd2b737ac6d39c49ced291beadaa7bcb9ebdd
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-03-08 01:57:24 +0100

    chore: Setup frontend with Nuxt 3 and TailwindCSS

---
+++ frontend/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-blue-500 text-white text-center">Tailwind fonctionne !</div>
+</template>
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss"], // Ajoute ce module officiel
+  devtools: { enabled: true },
+  compatibilityDate: "2025-03-08",
+});
