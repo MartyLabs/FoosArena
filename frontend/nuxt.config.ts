19608f57587d358bc7ca00d8cf53ba1d9b655ce0
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-03-09 14:15:00 +0100

    chore: configure Nuxt.js

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,14 @@
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"], // Ajoute ce module officiel
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/tailwind.css", "~/assets/css/toaster.css"],
+  postcss: {
+    plugins: {
+      "tailwindcss/nesting": {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   devtools: { enabled: true },
   compatibilityDate: "2025-03-08",
 });
+// Update Sat Mar 15 08:29:15 PM CET 2025
