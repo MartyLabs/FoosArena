d36a3fec29d7371dc2f6b45a03043bcff7da63b3
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-03-12 00:11:51 +0100

    tests: debug

--- backend/tests/team.test.js
+++ backend/tests/team.test.js
@@ -7,44 +7,6 @@ const prisma = new PrismaClient();
 let tournamentId, teamId;
 
 describe("Team API Tests", () => {
-  beforeAll(async () => {
-    await prisma.$executeRaw`TRUNCATE TABLE "Match", "Team", "Tournament" RESTART IDENTITY CASCADE;`;
-
-    await prisma.tournament.create({
-      data: { name: "Tournoi Test", date: new Date(), description: "Test" },
-    });
-
-    const tournament = await prisma.tournament.findFirst({
-      where: { name: "Tournoi Test" },
-    });
-
-    tournamentId = tournament ? tournament.id : null;
-
-    console.log("Tournament ID after creation:", tournamentId);
-  });
-
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
-
-  it("should add a team", async () => {
-    const teamName = "Clementine Team";
-
-    const res = await request(app).post("/teams").send({
-      name: teamName,
-      tournamentId,
-    });
-
-    console.log("Response from POST /teams:", res.body);
-
-    expect(res.statusCode).toBe(201);
-    console.log("Response from POST /teams:", res.body);
-    expect(res.body.name).toBe(teamName);
-    expect(res.body.id).toBeDefined();
-
-    teamId = res.body.id;
-  });
-
   it("should retrieve all the teams from a tournament", async () => {
     const res = await request(app).get(`/teams/${tournamentId}`);
     expect(res.statusCode).toBe(200);
